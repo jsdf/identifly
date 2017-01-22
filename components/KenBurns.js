4b1fe02c92e5dbfcf415cafcb442ef44108669de
James Friend <james@jsdf.co>
2017-01-22 13:09:34 -0800

    implement ken burns effect

--- components/KenBurns.js
+++ components/KenBurns.js
@@ -12,121 +12,186 @@ import {knuthShuffle} from 'knuth-shuffle';
 import speciesStyles from './speciesStyles';
 
 
+/*
+Orchestration:
+
+img1 load
+img1 fadeIn,
+img2 load, wait 5000
+img2 fadeIn
+img1 remove, img3 load, wait 5000
+img3 fadeIn
+img2 remove, img4 load, wait 5000
+img4 fadeIn
+etc.
+
+*/
+
+function wrap(val, max) {
+  return val <= max ? val : val - max;
+}
+
+function wait(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
 function initState(props) {
   return {
     shuffledImages: knuthShuffle(props.imageAssets),
-    imageIndex: 0,
-    zoomValue: new Animated.Value(0),
-    opacityValue: new Animated.Value(1),
+    images: [
+      imageState(0, props),
+    ],
   };
 }
 
-function intervalState(state, props) {
-  if (state.imageIndex < state.shuffledImages.length - 1) {
-    return {
-      ...state,
-      imageIndex: state.imageIndex + 1,
-    };
-  }
+function imageState(index, props) {
+  return {
+    index: wrap(index, props.imageAssets.length - 1),
+  };
+}
 
-  return initState(props);
+function updateOnImageShow(prevIndex, state, props) {
+  const images = [
+    imageState(prevIndex, props),
+    imageState(prevIndex + 1, props),
+  ];
+
+  return {
+    ...state,
+    images,
+  };
 }
 
 export default class KenBurns extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = initState(props);
-    this._zoom();
-  }
-
-  componentDidMount() {
-    this._interval = setInterval(() => {
-      this.setState(intervalState, () => 
-        this._zoom()
-      );
-    }, 5000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this._interval);
-  }
-
-  componentDidUpdate() {
-    const nextImage = this.state.shuffledImages[this.state.imageIndex + 1];
-    if (nextImage != null) {
-      Exponent.Asset.fromModule(nextImage).downloadAsync();
-    }
-  }
-
-  _zoom() {
-    this.state.zoomValue.setValue(1);
-    // return
-    Animated.timing(
-      this.state.zoomValue,
-      {
-        toValue: 1.2,
-        duration: 5000,
-        easing: Easing.in(Easing.ease),
-      },
-    ).start();
-    this.state.opacityValue.setValue(1);
-    // return
-    Animated.timing(
-      this.state.opacityValue,
-      {
-        toValue: 0,
-        delay: 3000,
-        duration: 2000,
-        easing: Easing.out(Easing.ease),
-      },
-    ).start();
+    this.state = initState(props); 
   }
 
   componentWillReceiveProps(nextProps) {
+    // reset if images change
     if (nextProps.imageAssets !== this.props.imageAssets) {
       this.setState(initState(nextProps));
-      this._zoom();
     }
   }
 
+  componentWillUnmount() {
+    this._unmounted = true;
+  }
+
   render() {
-    const imageAsset = this.state.shuffledImages[this.state.imageIndex];
-    const imageAssetNext = this.state.shuffledImages[Math.min(this.state.imageIndex, this.state.shuffledImages - 1)];
     const heightForWidth = this.props.aspectHeight / this.props.aspectWidth;
+    const height = heightForWidth * this.props.width;
+    const width = this.props.width;
+
+    const images = this.state.images.map(({index}) => {
+      const imageAsset = this.state.shuffledImages[index];
+      const first = !this._firstImageShown && index === 0;
+      return (
+        <KenBurnsImage
+          key={index}
+          index={index}
+          image={imageAsset}
+          height={height}
+          width={width}
+          delay={first ? 0 : 5000}
+          onShow={this._onImageShow}
+        />
+      );
+    });
 
     return (
       <View style={speciesStyles.container}>
         <View style={{
-          height: heightForWidth * this.props.width,
-          width: this.props.width,
+          height,
+          width,
           overflow: 'hidden',
+          position: 'relative',
         }}>
-          <Image
-            source={imageAssetNext}
-            style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              resizeMode: 'cover',
-              height: heightForWidth * this.props.width,
-              width: this.props.width,
-            }}
-          />
-          <Animated.Image
-            source={imageAsset}
-            style={{
-              resizeMode: 'cover',
-              height: heightForWidth * this.props.width,
-              width: this.props.width,
-              transform: [
-                {scale: this.state.zoomValue},
-              ],
-              opacity: this.state.opacityValue,
-            }}
-          />
+          {images}
         </View>
       </View>
+    );
+  }
+
+  _onImageShow = (index) => {
+    if (this._unmounted) return;
+    this._firstImageShown = true;
+    this.setState((state, props) => {
+      return updateOnImageShow(index, state, props);
+    });
+  };
+}
+
+class KenBurnsImage extends React.Component {
+  state = {
+    index: 0,
+    zoomValue: new Animated.Value(1),
+    opacityValue: new Animated.Value(0),
+  };
+
+  componentDidMount() {
+    this._run();
+  }
+
+
+  async _run() {
+    await Promise.all([
+      Exponent.Asset.fromModule(this.props.image).downloadAsync(),
+      wait(this.props.delay),
+    ]);
+    await this._show(this.state);
+    this.props.onShow(this.props.index);
+  }
+
+  async _show(state) {
+    const zoom = Animated.timing(
+      state.zoomValue,
+      {
+        toValue: 1.2,
+        duration: 7000,
+        easing: Easing.in(Easing.ease),
+      },
+    );
+
+    const fadeIn = Animated.timing(
+      state.opacityValue,
+      {
+        toValue: 1,
+        duration: 1000,
+        easing: Easing.out(Easing.ease),
+      },
+    );
+
+    await new Promise(resolve => {
+      fadeIn.start(resolve);
+      zoom.start();
+    });
+  }
+
+  render() {
+    const {
+      width,
+      height,
+      image,
+    } = this.props;
+
+    return (
+      <Animated.Image
+        source={image}
+        style={{
+          resizeMode: 'cover',
+          height,
+          width,
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          transform: [
+            {scale: this.state.zoomValue},
+          ],
+          opacity: this.state.opacityValue,
+        }}
+      />
     );
   }
 }
